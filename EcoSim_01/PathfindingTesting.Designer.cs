cb5a2c71055af471897db433d901d454d13c6d43
William Allison <papercrane1001@gmail.com>
2018-04-05 07:40:26 -0700

    Began work on pathfinding testing form

--- EcoSim_01/PathfindingTesting.Designer.cs
+++ EcoSim_01/PathfindingTesting.Designer.cs
@@ -28,11 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mapBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapBox
+            // 
+            this.mapBox.Location = new System.Drawing.Point(33, 24);
+            this.mapBox.Name = "mapBox";
+            this.mapBox.Size = new System.Drawing.Size(511, 511);
+            this.mapBox.TabIndex = 1;
+            this.mapBox.TabStop = false;
+            this.mapBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mapBox_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Directions:\r\nLoad a map.\r\nClick once to select starting position of ship.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // PathfindingTesting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 627);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mapBox);
+            this.Name = "PathfindingTesting";
             this.Text = "PathfindingTesting";
+            ((System.ComponentModel.ISupportInitialize)(this.mapBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox mapBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- EcoSim_01/PathfindingTesting.cs
+++ EcoSim_01/PathfindingTesting.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Xml.Serialization;
 
 namespace EcoSim_01
 {
     public partial class PathfindingTesting : Form
     {
+        Map testingMap;
+
+        XmlSerializer xs;
+
         public PathfindingTesting()
         {
             InitializeComponent();
+
+            openFileDialog1.ShowDialog();
+
+            if(openFileDialog1.FileName != "")
+            {
+                using (var sr = new System.IO.StreamReader(openFileDialog1.FileName))
+                {
+                    //buildingMap = (Map)xs.Deserialize(sr);
+                    testingMap.tileSet = (List<List<Tile>>)xs.Deserialize(sr);
+
+                    testingMap.RefreshMapImage();
+
+                    mapBox.Image = testingMap.fullMapImage;
+                }
+            }
+        }
+
+        private void mapBox_MouseClick(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
