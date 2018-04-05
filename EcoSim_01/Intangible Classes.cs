c81167e64f2f1c92bcdd862933235d44e65a1ae2
William Allison <papercrane1001@gmail.com>
2018-04-05 13:27:07 -0700

    Theoretically finished the basics of the pathfinding

--- EcoSim_01/Intangible Classes.cs
+++ EcoSim_01/Intangible Classes.cs
@@ -39,6 +39,20 @@ namespace EcoSim_01
             else if (i == 3) { return new Coordinates(c.x, c.y - 1); }
             else { return new Coordinates(c.x, c.y); }
         }
+        public Coordinates iterVect(int i)
+        {
+            if (i == 0) { return new Coordinates(x + 1, y); }
+            else if (i == 1) { return new Coordinates(x, y + 1); }
+            else if (i == 2) { return new Coordinates(x - 1, y); }
+            else if (i == 3) { return new Coordinates(x, y - 1); }
+            else { return new Coordinates(x, y); }
+        }
+
+        public bool Equals(Coordinates c)
+        {
+            if (x == c.x && y == c.y) { return true; }
+            else { return false; }
+        }
     }
 
     public class PathfindingTile
@@ -46,6 +60,9 @@ namespace EcoSim_01
         public Tile t;
         //vect is to record direction from which the tile was accessed
         public Coordinates vect;
+        //vect may be replaced by the following:
+        public int dir;
+        public Coordinates coord;
         public int counter;
         public bool touched = false;
         public bool destination;
@@ -109,9 +126,14 @@ namespace EcoSim_01
         public void PathFind(List<Tile> destList, Map map)
         {
             //List<List<PathfindingTile>> activeList = new List<List<PathfindingTile>>();
+            PathfindingTile startTile = new PathfindingTile();
             List<PathfindingTile> activeList = new List<PathfindingTile>();
             List<List<PathfindingTile>> fullList = new List<List<PathfindingTile>>();
             List<Tile> destinations = destList;
+
+            List<Coordinates> destCoords = new List<Coordinates>();
+
+            foreach(Tile d in destList) { destCoords.Add(d.coord); }
 
             //Build list<list> of entire map
             for (int i = 0; i < map.tileSet.Count(); i++)
@@ -127,20 +149,70 @@ namespace EcoSim_01
 
             while(destinations.Count() > 0)
             {
-                foreach (PathfindingTile p in activeList)
+                //foreach (PathfindingTile p in activeList)
+                for (int activeIndex = 0; activeIndex < activeList.Count(); activeIndex++)
                 {
-                    if(p.counter == 0)
+                    //PathfindingTile p = activeList[activeIndex];
+                    if (activeList[activeIndex].counter == 0)
                     {
-                        //
+                        //check if neighbor is in activelist.
+                            //If not, add to activelist and assign vector.
+                        //check if neighbor is a destination.  If so, remove from destination list
+
+
+                        //Check if neighbor is in activelist
+                        
+                        for (int i = 0; i<4; i++)
+                        {
+                            int xP = activeList[activeIndex].coord.iterVect(i).x;
+                            int yP = activeList[activeIndex].coord.iterVect(i).y;
+                            bool inListAlready = false;
+
+                            for(int checkIndex = 0; checkIndex < activeList.Count(); checkIndex++)
+                            {
+                                if (activeList[checkIndex].coord.Equals(activeList[activeIndex].coord.iterVect(i)))
+                                {
+                                    inListAlready = true;
+                                }
+                            }
+
+                            if (!inListAlready)
+                            {
+                                //add to active list
+                                activeList.Add(fullList[xP][yP]);
+                                //assign direction from which it was accessed
+                                activeList.Last().dir = i;
+                                
+                                //Check if it's a destination
+                                for(int destInd = 0; destInd < destCoords.Count(); destInd++)
+                                {
+                                    if (destCoords[destInd].Equals(activeList.Last().coord))
+                                    {
+                                        destCoords.RemoveAt(destInd);
+                                        break;
+                                    }
+                                }
+                            }
+                            //if (t.coord.Equals(fullList[xP][yP]))
+                            //{
+
+                            //}
+                        }
+                        
                     }
 
 
 
-                    p.counter--;
+                    activeList[activeIndex].counter--;
                 }
             }
             
         }
+
+        //public bool test(List<PathfindingTile> active, PathfindingTile current)
+        //{
+            
+        //}
     }
 
     public class Commodity
